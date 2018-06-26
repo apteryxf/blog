5b859dd26d7b01e0b81a3acbd98a9f0d08ec4f08
Yuki Nakamuraya <fourdim53@gmail.com>
2018-06-27 03:16:29 +0900

    set article routing and controller

---
+++ app/Http/Controllers/ArticlesController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Article;
+
+class ArticlesController extends Controller
+{
+    public function index()
+    {
+        $articles = Article::orderBy('id', 'desc')->paginate(5);
+        return view('articles.index', compact('articles'));
+    }
+
+    public function show($id)
+    {
+        $article = Article::findOrFail($id);
+        return view('articles.show', compact('article'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,12 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function()
+{
+    Route::resource('articles', 'Admin\ArticlesController');
+});
+
+Route::resource('articles', 'ArticlesController', ['only' => [
+    'index', 'show'
+]]);
