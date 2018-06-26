9a572a7ddc38e51f59f6a802fd745495a3f4aeb4
Yuki Nakamuraya <fourdim53@gmail.com>
2018-06-27 03:15:33 +0900

    edit controller according to error message

--- app/Http/Controllers/Admin/ArticlesController.php
+++ app/Http/Controllers/Admin/ArticlesController.php
@@ -8,27 +8,17 @@ use App\Article;
 
 class ArticlesController extends Controller
 {
-        @return \Illuminate\Http\Response
-
     public function index()
     {
         $articles = Article::orderBy('id', 'desc')->paginate(5);
         return view('admin.articles.index', compact('articles'));
     }
 
-        @return \Illuminate\Http\Response
-
     public function create()
     {
         return view('admin.articles.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $this->validate($request, $this->validateRules);
@@ -37,37 +27,18 @@ class ArticlesController extends Controller
         return redirect('admin/articles');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         $article = Article::findOrFail($id);
         return view('admin.articles.show', compact('article'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         $article = Article::findOrFail($id);
         return view('admin.articles.edit', compact('article'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         $this->validate($request, $this->validateRules);
@@ -79,12 +50,6 @@ class ArticlesController extends Controller
         return redirect('admin/articles');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         $article = Article::findOrFail($id);
