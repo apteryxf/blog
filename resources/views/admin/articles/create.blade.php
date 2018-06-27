01fbd94ef8c197b89de460ae4d8aba13b8f66c58
Yuki Nakamuraya <fourdim53@gmail.com>
2018-06-27 15:59:10 +0900

    add view files of managing blog pages

---
+++ resources/views/admin/articles/create.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.admin')
+@section('content')
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">新規作成</div>
+                <div class="panel-body">
+
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+
+                    {!! Form::open(['url' => 'admin/articles',
+                                'class' => 'form-horizontal',
+                                'id' => 'article-input']) !!}
+
+                    @include('admin.articles.fields')
+
+                    {!! Form::close() !!}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/admin/articles/edit.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.admin')
+@section('content')
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">編集</div>
+                <div class="panel-body">
+
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+
+                    {!! Form::model($article,
+                    ['url' => [
+                        'admin/articles', $article->id],
+                        'method' => 'PATCH',
+                        'class' => 'form-horizontal',
+                        'id' => 'article-input'
+
+                    ]) !!}
+                    @include('admin.articles.fields')
+
+                    {!! Form::close() !!}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
